4f67216e084aafae81a9d3fd9e29d8f011c80e71
Richie Bendall <richiebendall@gmail.com>
2021-01-08 20:14:28 +1300

    Meta tweaks
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 import setuptools
 
 setuptools.setup(
-	name="scratch-verify-python",
-	version="1.0.0",
-	description="Verify the ownership of a Scratch account.",
-	url="https://github.com/Richienb/scratch-verify-python",
-	author="Richie Bendall",
-	author_email="richiebendall@gmail.com",
-	license="MIT",
-	python_requires=">=3.6",
-	packages=setuptools.find_packages(),
-	long_description=open("readme.md", "r").read(),
-	long_description_content_type="text/markdown",
-	install_requires=open("requirements.txt", "r").read().split()
+	name = "scratch-verify-python",
+	version = "1.0.0",
+	description = "Verify the ownership of a Scratch account.",
+	url = "https://github.com/Richienb/scratch-verify-python",
+	keywords = "scratch verify account ownership user",
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Typing :: Typed"
+	],
+	author = "Richie Bendall",
+	author_email = "richiebendall@gmail.com",
+	license = "MIT",
+	python_requires = ">=3.6",
+	packages = setuptools.find_packages(),
+	long_description = open("readme.md", "r").read(),
+	long_description_content_type = "text/markdown",
+	install_requires = open("requirements.txt", "r").read().split()
 )
