75e5d94ca7b36b019bd97147ba815c709c53c3a9
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-03-09 18:15:21 -0600

    fix

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 import setuptools
 
-setuptools.setup(name="scratch-verify",
-                 version="1.0.1",
-                 description="Verify the ownership of a Scratch account.",
-                 url="https://github.com/Richienb/scratch-verify-python",
-                 keywords="scratch verify account ownership user",
-                 classifiers=[
-                     "Development Status :: 5 - Production/Stable",
-                     "Intended Audience :: End Users/Desktop",
-                     "License :: OSI Approved :: MIT License",
-                     "Natural Language :: English",
-                     "Operating System :: OS Independent",
-                     "Programming Language :: Python", "Typing :: Typed"
-                 ],
-                 author="Richie Bendall",
-                 author_email="richiebendall@gmail.com",
-                 license="MIT",
-                 python_requires=">=3.6",
-                 packages=setuptools.find_packages(),
-                 long_description=open("readme.md", "r").read(),
-                 long_description_content_type="text/markdown",
-                 install_requires=open("requirements.txt", "r").read().split())
+setuptools.setup(
+	name = "scratch-verify",
+	version = "1.0.1",
+	description = "Verify the ownership of a Scratch account.",
+	url = "https://github.com/Richienb/scratch-verify-python",
+	keywords = "scratch verify account ownership user",
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Typing :: Typed"
+	],
+	author = "Richie Bendall",
+	author_email = "richiebendall@gmail.com",
+	license = "MIT",
+	python_requires = ">=3.6",
+	packages = setuptools.find_packages(),
+	long_description = open("readme.md", "r").read(),
+	long_description_content_type = "text/markdown",
+	install_requires = open("requirements.txt", "r").read().split()
+)
